1ea05a485e96169bf5864dd4a833e41e9e29eccb
tirumaladevi30 <53849557+tirumaladevi30@users.noreply.github.com>
2022-02-03 15:39:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,35 @@
-require('rootpath')();
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const errorHandler = require('_middleware/error-handler');
+// program for a simple calculator
+let result;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+// take the operator input
+const operator = prompt('Enter operator ( either +, -, * or / ): ');
 
-// api routes
-app.use('/users', require('./users/users.controller'));
+// take the operand input
+const number1 = parseFloat(prompt('Enter first number: '));
+const number2 = parseFloat(prompt('Enter second number: '));
 
-// global error handler
-app.use(errorHandler);
+switch(operator) {
+    case '+':
+         result = number1 + number2;
+        console.log(`${number1} + ${number2} = ${result}`);
+        break;
 
-// start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
-app.listen(port, () => console.log('Server listening on port ' + port));
+    case '-':
+         result = number1 - number2;
+        console.log(`${number1} - ${number2} = ${result}`);
+        break;
+
+    case '*':
+         result = number1 * number2;
+        console.log(`${number1} * ${number2} = ${result}`);
+        break;
+
+    case '/':
+         result = number1 / number2;
+        console.log(`${number1} / ${number2} = ${result}`);
+        break;
+
+    default:
+        console.log('Invalid operator');
+        break;
+}
